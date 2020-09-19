04429fe142607ad11f48d64c6c6747798888232e
Sadman Sakib Jisan <jisan.thecoder@gmail.com>
2020-09-20 01:39:43 +0600

    modal final

--- Script/dividediv.js
+++ Script/dividediv.js
@@ -1,4 +1,8 @@
 var buttonID = 1;
+var getSpan;
+var width1 = 50;
+var width2 = 50;
+
 function addElement(){
 
     var display = document.getElementById('display');
@@ -13,7 +17,7 @@ function addElement(){
     subDiv.setAttribute("class", "sub-container");
 
     span.setAttribute("class", "button" + buttonID);
-    span.setAttribute("onclick", "addColumn(this)");
+    span.setAttribute("onclick", "displayModal(this)");
 
     span.appendChild(plus);
     subDiv.append(span);
@@ -24,16 +28,19 @@ function addElement(){
 
 }
 
-function addColumn(getSpan) {
+function addColumn() {
 
-    displayModal();
+    document.getElementById('width1').value = '50';
+    document.getElementById('width2').value = '50';
+    document.getElementById('w1').innerHTML = 50 + '%';
+    document.getElementById('w2').innerHTML = 50 + '%';
 
-    width1 = parseInt(width1);
-    width2 = parseInt(width2);
+    modal.style.display = "none";
 
     var getID = getSpan.className;
     var getElement = document.getElementById(getID);
 
+    console.log(getID, getElement);
     getElement.innerHTML = '';
 
     var mainDiv1 = document.createElement("div");
@@ -57,14 +64,14 @@ function addColumn(getSpan) {
     mainDiv1.setAttribute("id", "button" + buttonID);
     subDiv1.setAttribute("class", "sub-container");
     span1.setAttribute("class", "button" + buttonID);
-    span1.setAttribute("onclick", "addColumn(this)");
+    span1.setAttribute("onclick", "displayModal(this)");
     span1.appendChild(plus1);
     buttonID++;
 
     mainDiv2.setAttribute("id", "button" + buttonID);
     subDiv2.setAttribute("class", "sub-container");
     span2.setAttribute("class", "button" + buttonID);
-    span2.setAttribute("onclick", "addColumn(this)");
+    span2.setAttribute("onclick", "displayModal(this)");
     span2.appendChild(plus2);
     buttonID++;
     
@@ -76,6 +83,28 @@ function addColumn(getSpan) {
 
     getElement.append(mainDiv1);
     getElement.append(mainDiv2);
+
+    width1 = 50;
+    width2 = 50;
+
+}
+
+function widthRange(modalInput){
+    var modalInputID = modalInput.id;
+    var widthValue1 = parseInt(modalInput.value);
+    if (modalInputID == 'width1'){
+        document.getElementById('width2').value = "" + 100 - widthValue1 + "";
+        document.getElementById('w1').innerHTML = widthValue1 + '%';
+        document.getElementById('w2').innerHTML = 100 - widthValue1 + '%';
+        width1 = widthValue1;
+        width2 = 100 - widthValue1;
+    } else {
+        document.getElementById('width1').value = "" + 100 - widthValue1 + "";
+        document.getElementById('w2').innerHTML = widthValue1 + '%';
+        document.getElementById('w1').innerHTML = 100 - widthValue1 + '%';
+        width2 = widthValue1;
+        width1 = 100 - widthValue1;
+    }
 
 }
 
@@ -91,18 +120,31 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-function displayModal() {
+function displayModal(getValue) {
     modal.style.display = "block";
+    getSpan = getValue;
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (cancel), close the modal
 span.onclick = function () {
     modal.style.display = "none";
+    document.getElementById('width1').value = '50';
+    document.getElementById('width2').value = '50';
+    document.getElementById('w1').innerHTML = 50 + '%';
+    document.getElementById('w2').innerHTML = 50 + '%';
+    width1 = 50;
+    width2 = 50;
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        document.getElementById('width1').value = '50';
+        document.getElementById('width2').value = '50';
+        document.getElementById('w1').innerHTML = 50 + '%';
+        document.getElementById('w2').innerHTML = 50 + '%';
+        width1 = 50;
+        width2 = 50;
     }
 }
