d94598455b2c5bf00395421c62a1ba89c6a75dc5
TeamBinaryBeast <binarybeastjr@gmail.com>
2020-09-20 12:54:59 +0600

    export

--- Script/export.js
+++ Script/export.js
@@ -1,28 +1,10 @@
 var display = document.getElementById("display");
-// function exportAsHtml() {
-//     raw = String(display.innerHTML);
-//     Exp(raw);
-// }
-// function Exp(raw) {
-//     try {
-//         var Div = document.getElementById("Exported");
-//         Div.remove();
-//         CreateExpo(raw);
-//     } catch (pass) {
-//         CreateExpo(raw);
-//     }
-// }
 
-// function CreateExpo(raw) {
-//     var Div = document.createElement("div");
-//     var node = document.createTextNode(raw);
-//     Div.appendChild(node);
-//     document.getElementsByClassName('sidebar')[0].appendChild(Div).setAttribute("id", "Exported");
-// }
 function exportAsHtml() {
     raw = display.innerHTML;
     exportInDiv(raw);
 }
+
 function exportInDiv(raw) {
     var Div = document.createElement("div");
     Div.innerHTML = raw;
@@ -31,23 +13,27 @@ function exportInDiv(raw) {
     removeAll(list);
     replicate();
 }
+
 function removeAll(list) {
     for (var x = 0; x < list.length; x++){
         list[x].remove();
     }
 }
+
 function replicate() {
     var exported = document.querySelector('#Exported');
     fixedHTML = String(exported.innerHTML);
     document.querySelector('#Exported').remove();
     finalExp(fixedHTML);
 }
+
 function createExpo(fixedHTML) {
     var Div = document.createElement("div");
     var node = document.createTextNode(fixedHTML);
     Div.appendChild(node);
     document.getElementsByClassName('sidebar')[0].appendChild(Div).setAttribute("id", "FinalExp");
 }
+
 function finalExp(fixedHTML) {
     try {
         var Div = document.getElementById("FinalExp");
